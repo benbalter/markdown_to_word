26bfa49a2c850edbe087b1ec7510fe17e381a88c
Ben Balter <ben.balter@github.com>
2015-05-28 10:48:08 -0400

    add test for the template

--- spec/markdown_to_word_spec.rb
+++ spec/markdown_to_word_spec.rb
@@ -8,4 +8,10 @@ describe MarkdownToWord do
       expect(WordToMarkdown.new(file).to_s).to eql("# Test")
     end
   end
+
+  it "returns the default template" do
+    template = MarkdownToWord.default_template
+    expect(template).to eql(File.expand_path("../lib/markdown_to_word/template.docx", File.dirname(__FILE__)))
+    expect(File.exists?(template)).to eql(true)
+  end
 end
